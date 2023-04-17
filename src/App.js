6843181814f8a324739e9255fdbec631f3606412
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-17 11:15:29 +0300

    feat: initialize Login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/signup-page/Login";
+
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
