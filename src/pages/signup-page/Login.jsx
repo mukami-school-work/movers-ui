21472cf2a6cd2db1307f619dd318bb77a347736d
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-17 13:51:17 +0300

    feat: Add Login component with email and password state

--- src/pages/signup-page/Login.jsx
+++ src/pages/signup-page/Login.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState } from 'react';
+import { MailIcon, KeyIcon } from '@heroicons/react/solid';
 
-const Login = () => {
+
+  function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+  
+    const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+    };
+  
+    const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      // Email validation
+      const validEmailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+      if (!validEmailRegex.test(email)) {
+        setEmailError('Invalid email address');
+        return;
+      }
+  
+      console.log('Email:', email);
+      console.log('Password:', password);
+    };
+
   return (
     <div>Login</div>
-  )
+  );
 }
 
 export default Login
