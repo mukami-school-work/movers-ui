d6b12635deaead7356e4ed183d12359d5cc513a5
lizdmukami <lizdmukami@gmail.com>
2023-04-17 20:38:04 +0300

    feat: listings page skeleton with tailwind css styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListingsPage from "./pages/listings page/ListingsPage";
+
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/movers" element={<ListingsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
 export default App;
---
+++ src/pages/listings page/ListingsPage.jsx
@@ -0,0 +1,170 @@
+const ListingsPage = () => {
+  return (
+    <div className="relative bg-azure w-full md:shrink-1 h-[1024px] overflow-visible flex flex-col flex-1 items-center justify-start text-center text-[18px] text-body-text-100 md:justify-items-center sm: justify-items-center font-open-sans">
+      <div className="bg-azure flex flex-col items-center justify-center">
+        <div className="relative w-[1040px] h-[720px] shrink-1">
+          <div className="absolute top-[75px] left-[0px] w-[1040px] h-[645px] flex flex-col items-center justify-end gap-[26px]">
+            <div className="self-auto flex-1 relative leading-[27px]">
+              At Movers we pride in offering the best moving solutions based on
+              your needs and location. We have partnered with some of the best
+              movers, with very high ratings in Nairobi. To get you started
+              choose from the list below and click read more to view more about
+              each company.
+            </div>
+            <div className="self-auto flex flex-col items-center justify-center text-left text-8xl text-darkslategray-200 font-inter">
+              <div className="rounded-3xs bg-white w-[1040px] flex flex-col py-[23px] px-[19px] box-border items-end justify-start">
+                <div className="self-stretch flex flex-col items-start justify-start z-[0]">
+                  <div className="w-[1002px] h-[499px] shrink-1 flex flex-col items-end justify-start gap-[9px]">
+                    <div className="flex flex-row items-start justify-start">
+                      <div className="w-[1002px] h-[214px] shrink-1 flex flex-col pt-[5px] px-0 pb-0 box-border items-end justify-end relative">
+                        <div className="flex flex-row items-start justify-start z-[0]">
+                          <div className="h-[214px] flex flex-row items-center justify-start">
+                            <div className="w-[202px] h-[214px] shrink-1 flex flex-col py-0 pr-[18px] pl-0 box-border items-start justify-end">
+                              <div className="w-[184px] h-[214px] shrink-1 flex flex-col items-start justify-end gap-[6px]">
+                                <img
+                                  className="relative w-[182px] h-[149px] shrink-0 object-cover"
+                                  alt=""
+                                  src="/divcardimage@2x.png"
+                                />
+                                <div className="w-[184px] flex flex-col items-center justify-center">
+                                  <div className="rounded-89xl bg-white shadow-[0px_4px_2px_rgba(120,_116,_104,_0.04)] flex flex-row py-[9px] pr-[18.28000259399414px] pl-[18.270000457763672px] items-start justify-start border-[1px] border-solid border-whitesmoke-200">
+                                    <div className="relative w-[147.45px] h-[41px] shrink-0">
+                                      <div className="absolute top-[calc(50%_-_5.94px)] left-[39.45px]" />
+                                      <div className="absolute top-[-1px] left-[0px] w-[155.73px] h-[41px] flex flex-row items-center justify-start gap-[7px]">
+                                        <div className="relative tracking-[-1px] leading-[41px]">
+                                          4.4
+                                        </div>
+                                        <div className="w-[108px] h-8 shrink-0 flex flex-col items-start justify-start gap-[4px] text-xs text-black">
+                                          <img
+                                            className="relative w-[108px] h-4 shrink-0 object-cover"
+                                            alt=""
+                                            src="/45removebgpreview1e1625735940152png@2x.png"
+                                          />
+                                          <div className="relative tracking-[-1px] leading-[41px] flex items-center w-[97px] h-3 shrink-0">
+                                            <span className="[line-break:anywhere] w-full">
+                                              <b>{`Movers `}</b>
+                                              <span className="text-red">
+                                                Home
+                                              </span>
+                                            </span>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="w-[800px] h-[214px] shrink-0 flex flex-col py-[7px] px-0 box-border items-center justify-start text-smi text-gray">
+                              <div className="w-[800px] h-[176.99px] shrink-0 flex flex-col items-start justify-start gap-[12px]">
+                                <div className="w-[281px] h-[82px] shrink-0 flex flex-col items-start justify-start gap-[16px]">
+                                  <div className="rounded-sm bg-whitesmoke-100 flex flex-row pt-2 pb-1.5 pr-[17.19000244140625px] pl-3 items-start justify-start">
+                                    <b className="relative leading-[18px] uppercase">
+                                      Best for Overall
+                                    </b>
+                                  </div>
+                                  <b className="relative text-6xl leading-[33.8px] text-darkslategray-200">
+                                    International Van Lines
+                                  </b>
+                                </div>
+                                <div className="flex flex-row py-0 pr-[413.6000061035156px] pl-0 items-start justify-start text-xs text-darkslategray-200">
+                                  <div className="h-[82.19px] flex flex-row items-center justify-start">
+                                    <div className="box-border w-[97px] h-[82.19px] shrink-0 flex flex-col items-start justify-start gap-[16px] border-r-[1px] border-solid border-gainsboro">
+                                      <div className="w-[87px] h-12 shrink-0 flex flex-col pt-1 pb-[5px] pr-[39.439998626708984px] pl-0 box-border items-start justify-end gap-[9px] opacity-[0.6]">
+                                        <div className="relative leading-[24px] flex items-center w-[43.62px] h-[15px] shrink-0">
+                                          Deposit
+                                        </div>
+                                        <div className="relative leading-[24px] flex items-center w-[47.56px] h-[15px] shrink-0">
+                                          required
+                                        </div>
+                                      </div>
+                                      <div className="relative text-smi leading-[18.2px] flex items-center w-[22.48px] h-[18px] shrink-0">
+                                        Yes
+                                      </div>
+                                    </div>
+                                    <div className="box-border w-[147.95px] h-[82.19px] shrink-0 flex flex-col py-1 px-5 items-start justify-start gap-[21px] border-r-[1px] border-solid border-gainsboro">
+                                      <div className="relative leading-[24px] flex items-center w-[107.15px] h-[15px] shrink-0 opacity-[0.6]">
+                                        Service availability
+                                      </div>
+                                      <div className="relative text-sm leading-[18.2px] flex items-center w-[59.81px] h-[18px] shrink-0">
+                                        Excellent
+                                      </div>
+                                    </div>
+                                    <div className="flex flex-col pt-0 pb-[24.19000244140625px] pr-[16.449996948242188px] pl-5 items-start justify-start gap-[15px] text-2xs">
+                                      <div className="relative leading-[24px] opacity-[0.6]">
+                                        Full-service packing
+                                      </div>
+                                      <div className="relative text-smi leading-[18.2px]">
+                                        Yes
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="absolute my-0 mx-[!important] top-[5px] left-[722px] w-[280px] h-[97px] shrink-0 z-[1] text-center text-xl">
+                          <b className="absolute top-[82px] left-[97px] leading-[15px]">
+                            Ksh 5000
+                          </b>
+                          <div className="absolute top-[0px] left-[0px] bg-white flex flex-row items-start justify-start text-5xl text-white">
+                            <div className="rounded-lg bg-mediumspringgreen shadow-[0px_4px_12px_rgba(0,_0,_0,_0.2)] w-[280px] h-[62px] shrink-0 flex flex-col items-center justify-center z-[0]">
+                              <b className="relative leading-[28px] flex items-center justify-center w-[118px] h-10 shrink-0">
+                                Book Now
+                              </b>
+                            </div>
+                            <img
+                              className="absolute my-0 mx-[!important] top-[21px] right-[21px] w-[22px] h-5 shrink-0 overflow-hidden z-[1]"
+                              alt=""
+                              src="/svgabsolute.svg"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="w-[991px] flex flex-col items-center justify-start text-smi text-black">
+                      <div className="flex flex-row pt-px px-0 pb-0 items-start justify-start border-t-[1px] border-solid border-whitesmoke-300">
+                        <div className="flex flex-row py-4 px-0 items-start justify-start gap-[856px]">
+                          <div className="relative leading-[30px] font-medium z-[1]">
+                            Services Offered
+                          </div>
+                          <img
+                            className="relative w-[30px] h-[30px] shrink-0 overflow-hidden z-[0]"
+                            alt=""
+                            src="/image.svg"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex flex-row pt-px px-0 pb-0 items-start justify-start text-[inherit] font-inherit border-t-[1px] border-solid border-whitesmoke-300">
+                        <div className="w-[991px] shrink-0 flex flex-row py-4 px-0 box-border items-start justify-start">
+                          <div className="relative leading-[30px] font-medium z-[0]">
+                            <ul className="m-0 pl-[17px]">
+                              <li className="mb-0">Full-service packing</li>
+                              <li className="mb-0">Shipment tracking</li>
+                              <li className="mb-0">
+                                Several moving insurance options
+                              </li>
+                              <li className="mb-0">Vehicle shipping</li>
+                              <li className="mb-0">Specialty moving</li>
+                              <li>Furniture disassembly and reassembly</li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="absolute top-[0px] left-[134px] text-21xl leading-[46px] font-dm-serif-display-40-sp-h3 text-heading-text inline-block w-[785.3px] h-[29.33px]">
+            Movers Companies Based in Nairobi...
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListingsPage;
