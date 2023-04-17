b3ed4355e5e871ee84976003e9fece7f891ea1ca
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-17 14:35:19 +0300

    fix: Add MailIcon and KeyIcon components

--- src/pages/signup-page/Login.jsx
+++ src/pages/signup-page/Login.jsx
@@ -45,7 +45,8 @@ import { MailIcon, KeyIcon } from '@heroicons/react/solid';
                 Email
               </label>
               <div className="relative w-full">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none          
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          
                 <MailIcon className="h-5 w-5 text-green-500" /> 
                   </div>
                 <input
