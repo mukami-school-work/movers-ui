cd1aeed2ba9a53444692e9f0921ebfcaaf5bfc72
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-17 14:20:15 +0300

    feat: Add login form with validations and styles

--- src/pages/signup-page/Login.jsx
+++ src/pages/signup-page/Login.jsx
@@ -39,10 +39,56 @@ import { MailIcon, KeyIcon } from '@heroicons/react/solid';
         </div>
         <div className="w-1/2 p-10 flex flex-col items-center">
           <h1 className="text-3xl font-bold mb-6 text-green-600">Welcome Back</h1>
+          <form onSubmit={handleSubmit} className="w-full">
+            <div className="mb-4 w-full">
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
+              <div className="relative w-full">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  
+                </div>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  className={`block w-full py-3 px-4 pl-10 rounded-lg bg-green-100 focus:outline-none focus:bg-green-200 transition duration-200 ${emailError ? 'border-red-500' : ''}`}
+                />
+                {emailError && (
+                  <span className="text-red-500 text-sm">{emailError}</span>
+                )}
+              </div>
+            </div>
+            <div className="mb-4 w-full">
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <div className="relative w-full">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  
+                </div>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="block w-full py-3 px-4 pl-10 rounded-lg bg-green-100 focus:outline-none focus:bg-green-200 transition duration-200"
+                />
+              </div>
+            </div>
+            <button type="submit" className="block w-full py-3 px-4 rounded-lg bg-green-600 text-white font-bold text-lg hover:bg-green-700 transition duration-200">
+              Log In
+            </button>
+          </form>
           </div>
           </div>
           </div>
-          
+
   );
 }
 
