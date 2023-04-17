212024f9fb577959a06d6f25ae21f48f9a72bcb8
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-17 13:58:56 +0300

    feat: Add email and password validation to handleSubmit()

--- src/pages/signup-page/Login.jsx
+++ src/pages/signup-page/Login.jsx
@@ -5,7 +5,7 @@ import { MailIcon, KeyIcon } from '@heroicons/react/solid';
   function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
+    const [emailError, setEmailError] = useState
   
     const handleEmailChange = (e) => {
       setEmail(e.target.value);
@@ -28,6 +28,8 @@ import { MailIcon, KeyIcon } from '@heroicons/react/solid';
       console.log('Email:', email);
       console.log('Password:', password);
     };
+    
+    
 
   return (
     <div>Login</div>
