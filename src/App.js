f6477420a31059414a24309a13a9bd381293d2b6
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-18 04:51:30 +0300

    feat:add route path to Login compmonent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/signup-page/Login";
-
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/signin-page/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
+    {/* <Login /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          {/* <Route path="/login" element={<Login />} /> */}
         </Routes>
       </BrowserRouter>
     </>
