b1a66b6ee0a2dce0845bc977a6e39c66f90ee079
lizdmukami <lizdmukami@gmail.com>
2023-04-19 01:36:09 +0300

    feat: reviews page skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Hero, Navbar } from "components";
 import { Login } from "pages";
+import Reviews from "pages/reviews-page/Reviews";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/reviews" element={<Reviews />} />
         </Routes>
       </BrowserRouter>
     </>
