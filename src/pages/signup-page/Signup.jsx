f204e5f4d5bc1317c0abdfab5955bef4da4f914e
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-18 09:33:46 +0300

    feat:Import useState, useForm hooks and heroicons

--- src/pages/signup-page/Signup.jsx
+++ src/pages/signup-page/Signup.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-
+import { useState } from 'react';
+import { MailIcon, KeyIcon, PhoneIcon, UserIcon, PhotographIcon } from '@heroicons/react/solid';
+import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline';
+import { useForm } from 'react-hook-form';
 function Signup() {
   return (
     <div>Signup</div>
