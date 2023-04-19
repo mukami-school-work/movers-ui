8742538e94f98bfe88a36463a8084c4952d6063f
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-19 03:56:21 +0300

    ft:add search functionality, styling and descriptions

--- src/pages/search-page/Search.jsx
+++ src/pages/search-page/Search.jsx
@@ -63,6 +63,27 @@ function Search() {
         <h1 className="text-4xl text-green-600 text-center mb-6">
           What would you like to move?
         </h1>
+        <p className="text-center max-w-lg text-gray-600 text-sm">
+          To get the most precise price range, we recommend going room by room and
+          confirming all items you wish to move.
+        </p>
+        <form onSubmit={handleSearch} className="mt-8 w-full max-w-lg">
+          <div className="relative">
+            <input
+              type="text"
+              className="block w-full bg-white border-b-4 rounded-lg py-3 px-4 pr-10 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+              placeholder="Search items in your apartment"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="absolute right-0 top-0 h-full w-14 text-green-500 flex items-center justify-center focus:outline-none"
+            >
+              <SearchIcon className="h-12 w-12" />
+</button>
+</div>
+</form>
         </div>
     </div>
   )
