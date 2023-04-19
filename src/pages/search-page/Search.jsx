20bd886a45185c2be69289cc16c9c944cf96b594
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-19 03:32:40 +0300

    ft:add cart management functionality

--- src/pages/search-page/Search.jsx
+++ src/pages/search-page/Search.jsx
@@ -2,10 +2,34 @@ import { useState } from 'react';
 
 function Search() {
     const [searchQuery, setSearchQuery] = useState('');
- 
+    const [cartItems, setCartItems] = useState([]);
+    const [cartVisible, setCartVisible] = useState(false);
+
   const handleSearch = (e) => {
     e.preventDefault();
     // handle search logic here
+  };
+
+  const handleCartToggle = () => {
+    setCartVisible(!cartVisible);
+  };
+
+  const handleAddToCart = (item) => {
+    const index = cartItems.findIndex((cartItem) => cartItem.id === item.id);
+    if (index >= 0) {
+      // item already exists in cart, update quantity
+      const newCartItems = [...cartItems];
+      newCartItems[index].quantity += item.quantity;
+      setCartItems(newCartItems);
+    } else {
+      // item doesn't exist in cart, add to cart
+      setCartItems([...cartItems, item]);
+    }
+  };
+
+  const handleRemoveFromCart = (item) => {
+    const newCartItems = cartItems.filter((cartItem) => cartItem.id !== item.id);
+    setCartItems(newCartItems);
   };
   return (
     <div>Search</div>
