f48d66c9044254ddfe27e67b47c015a05a5dc6cc
jos-the-top-dev <devs.josia@gmail.com>
2023-04-20 09:31:21 +0300

    fix: resize the profile images to a bigger size

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,11 +5,11 @@ function Team() {
     <div class="max-w-5xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
       {/* <!-- Title --> */}
       <div className="flex flex-col items-center mb-16">
-        <p className="inline-block px-3 py-px text-lg font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        <p className="inline-block px-3 py-px text-xl font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
           The team
         </p>
         <div className="max-w-3xl font-bold leading-none tracking-tight text-gray-900 md:mx-auto">
-          <span className="text-5xl">Meet the creative minds</span>
+          <span className="text-5xl">Our company</span>
         </div>
       </div>
       {/* <!-- End Title --> */}
@@ -18,7 +18,7 @@ function Team() {
       <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-8 md:gap-12">
         <div class="text-center">
           <img
-            class="rounded-full w-24 h-24 mx-auto"
+            class="rounded-full w-48 h-48 mx-auto"
             src="https://images.unsplash.com/photo-1602452920335-6a132309c7c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
             alt=""
           />
@@ -35,7 +35,7 @@ function Team() {
 
         <div class="text-center">
           <img
-            class="rounded-full w-24 h-24 mx-auto"
+            class="rounded-full w-48 h-48 mx-auto"
             src="https://images.unsplash.com/photo-1514846226882-28b324ef7f28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
             alt=""
           />
@@ -52,7 +52,7 @@ function Team() {
 
         <div class="text-center">
           <img
-            class="rounded-full w-24 h-24 mx-auto"
+            class="rounded-full w-48 h-48 mx-auto"
             src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
             alt=""
           />
@@ -69,7 +69,7 @@ function Team() {
 
         <div class="text-center">
           <img
-            class="rounded-full w-24 h-24 mx-auto"
+            class="rounded-full w-48 h-48 mx-auto"
             src="https://images.unsplash.com/photo-1520409364224-63400afe26e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
             alt=""
           />
@@ -86,7 +86,7 @@ function Team() {
 
         <div class="text-center">
           <img
-            class="rounded-full w-24 h-24 mx-auto"
+            class="rounded-full w-48 h-48 mx-auto"
             src="https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
             alt=""
           />
@@ -103,7 +103,7 @@ function Team() {
 
         <div class="text-center">
           <img
-            class="rounded-full w-24 h-24 mx-auto"
+            class="rounded-full w-48 h-48 mx-auto"
             src="https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
             alt=""
           />
