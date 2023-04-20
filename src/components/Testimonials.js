6ec7013197e4a2f9d526bb65d5768a5e5f2025ad
jos-the-top-dev <devs.josia@gmail.com>
2023-04-20 09:39:49 +0300

    fix: add a bit of rasmataz to the section title

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -7,15 +7,44 @@ const Testimonials = () => {
           <div class="bg-purple-200 opacity-20 blur-3xl w-[1036px] h-[300px] dark:bg-purple-900 dark:opacity-20"></div>
         </div>
         {/* <!-- End Gradients --> */}
-        <div className="flex flex-col items-center">
+        <div className="max-w-xl my-10 md:mx-auto sm:text-center lg:max-w-3xl md:mb-12">
           <div>
-            <p className="inline-block px-3 py-px text-lg font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-              Testimonials
+            <p className="inline-block px-3 py-px mb-4 text-xl font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+              Stories
             </p>
           </div>
-          <div className="max-w-3xl mb-10 font-bold leading-none tracking-tight text-gray-900 lg:text-5xl sm:text-4xl md:mx-auto">
-            <span className="text-5xl">Stories from our customers</span>
-          </div>
+          <h2 className="max-w-3xl mb-6 font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            <span className="relative inline-block">
+              <svg
+                viewBox="0 0 52 24"
+                fill="currentColor"
+                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+              >
+                <defs>
+                  <pattern
+                    id="18302e52-9e2a-4c8e-9550-0cbb21b38e55"
+                    x="0"
+                    y="0"
+                    width=".135"
+                    height=".30"
+                  >
+                    <circle cx="1" cy="1" r=".7" />
+                  </pattern>
+                </defs>
+                <rect
+                  fill="url(#18302e52-9e2a-4c8e-9550-0cbb21b38e55)"
+                  width="52"
+                  height="24"
+                />
+              </svg>
+              <span className="relative text-5xl">Testimonies</span>
+            </span>{" "}
+            <span className="text-5xl">from customers</span>
+          </h2>
+          <p className="font-medium text-gray-700 lg:text-lg md:text-sm">
+            We not only ensure your confidence in us, we also go miles to
+            inspire belief so that you will have to come again.
+          </p>
         </div>
         {/* <!-- Grid --> */}
         <div class="lg:grid lg:grid-cols-6 lg:gap-8 rounded-xl">
