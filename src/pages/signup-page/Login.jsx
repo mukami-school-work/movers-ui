54e69b1d2d166c32026d5cc4170c3f630b0c8e07
jos-the-top-dev <devs.josia@gmail.com>
2023-04-22 00:44:55 +0300

    fix: redesign the login UI

--- src/pages/signup-page/Login.jsx
+++ src/pages/signup-page/Login.jsx
@@ -1,10 +1,13 @@
-import { KeyIcon, MailIcon } from "@heroicons/react/solid";
 import { useState } from "react";
 
-function Login() {
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -16,103 +19,163 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Email validation
-    const validEmailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    if (!validEmailRegex.test(email)) {
-      setEmailError("Invalid email address");
-      return;
-    }
-
     console.log("Email:", email);
     console.log("Password:", password);
   };
 
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <div className="flex flex-col w-full mx-auto bg-white rounded-lg shadow-md lg:flex-row">
-        <div className="w-full p-10 lg:w-1/2">
-          <img
-            src="/signin.jpg"
-            alt="Login"
-            className="object-cover w-full h-full rounded-lg"
-          />
-        </div>
-        <div className="flex flex-col items-center w-full p-10 lg:w-1/2">
-          <h1 className="mb-6 text-3xl font-bold text-black">Welcome Back</h1>
-          <form onSubmit={handleSubmit} className="w-full">
-            <div className="w-full mb-4">
-              <label htmlFor="email" className="sr-only">
-                Email
-              </label>
-              <div className="relative w-full">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <MailIcon className="w-5 h-5 text-green-500" />
-                </div>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={handleEmailChange}
-                  className={`block w-full py-3 px-4 pl-10 focus:outline-none focus:bg-green-50 focus:placeholder-transparent border-b-2 border-green-200 transition duration-200 ${
-                    emailError ? "border-red-500" : ""
-                  }`}
-                />
-                {emailError && (
-                  <span className="text-sm text-red-500">{emailError}</span>
-                )}
-              </div>
-            </div>
-            <div className="w-full mb-4">
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <div className="relative w-full">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <KeyIcon className="w-5 h-5 text-green-500" />
-                </div>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  className="block w-full px-4 py-3 pl-10 transition duration-200 border-b-2 border-green-200 focus:outline-none focus:bg-green-50 focus:placeholder-transparent"
-                />
-              </div>
-            </div>
-            <button
-              type="submit"
-              className="block w-full px-4 py-3 mt-4 text-lg font-bold text-center text-white transition duration-200 bg-green-600 rounded-lg hover:bg-green-700"
-            >
-              Log In
-            </button>
-          </form>
-          <div className="flex items-center justify-between w-full mt-4">
-            <a
-              href="/forgot-password"
-              className="text-green-600 hover:underline"
-            >
-              Forgot Password
-            </a>
+    <section>
+      <div className="flex items-center justify-center overflow-hidden min-">
+        <div className="flex flex-col px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="w-full max-w-xl mx-auto lg:w-96">
             <div>
-              <p>
-                <a href="/signup" className="text-green-600 hover:underline">
-                  <span className="mb-2 text-right text-gray-600">
-                    Don't have an account?
-                  </span>{" "}
-                  Sign Up
-                </a>
-              </p>
+              <a href="/" className="font-bold text-primary-green text-medium">
+                Movers
+              </a>
+              <h2 className="mt-6 text-3xl font-extrabold text-neutral-600">
+                Sign in.
+              </h2>
+            </div>
+
+            <div className="mt-8">
+              <div className="mt-6">
+                <form action="#" onSubmit={handleSubmit} className="space-y-6">
+                  {/* Email Field */}
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-neutral-600"
+                    >
+                      {" "}
+                      Email address{" "}
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        onChange={handleEmailChange}
+                        required=""
+                        placeholder="Your Email"
+                        className="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg text-neutral-600 bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                      />
+                    </div>
+                  </div>
+
+                  {/* Password Field */}
+                  <div className="space-y-1">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-neutral-600"
+                    >
+                      {" "}
+                      Password{" "}
+                    </label>
+                    <div className="flex mt-1">
+                      <input
+                        id="password"
+                        name="password"
+                        type={passwordVisible ? "text" : "password"}
+                        onChange={handlePasswordChange}
+                        autoComplete="current-password"
+                        required=""
+                        placeholder="Your Password"
+                        className="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-s-lg text-neutral-600 bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                      />
+                      <button
+                        onClick={togglePasswordVisibility}
+                        className="px-4 transition duration-500 ease-in-out transform border-none bg-gray-50 rounded-e-lg hover:cursor-pointer hover:bg-gray-100"
+                      >
+                        {passwordVisible ? (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6 text-primary-green"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6 text-primary-green"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                            />
+                          </svg>
+                        )}
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="flex items-center justify-between">
+                    <div className="text-sm">
+                      <a
+                        href="/login"
+                        className="font-medium text-primary-green hover:text-green-500"
+                      >
+                        {" "}
+                        Forgot your password?{" "}
+                      </a>
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform border-none bg-primary-green rounded-xl hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+                </form>
+
+                <div className="relative my-4">
+                  <div className="absolute inset-0 flex items-center">
+                    <div className="w-full border-t border-gray-300"></div>
+                  </div>
+                  <div className="relative flex justify-center text-sm">
+                    <span className="px-2 bg-white text-neutral-600">
+                      {" "}
+                      Or create account{" "}
+                    </span>
+                  </div>
+                </div>
+
+                <div>
+                  <a
+                    href="/signup"
+                    className="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center transition duration-500 ease-in-out transform border-none bg-black-800 text-primary-green rounded-xl hover:bg-black-600 hover:text-primary-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  >
+                    <div className="flex items-center justify-center">
+                      <span className="ml-4"> Get Started</span>
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
-
-export default Login;
