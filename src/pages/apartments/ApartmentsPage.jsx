9e5b2945985c75d13ff8214ab6161d952c00d698
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-24 16:16:50 +0300

    feat:add styles to the next button

--- src/pages/apartments/ApartmentsPage.jsx
+++ src/pages/apartments/ApartmentsPage.jsx
@@ -26,33 +26,33 @@ function ApartmentsPage() {
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl text-green-500 mb-4 mt-8">
+      <h1 className="text-4xl text-primary-green mb-4 pt-6 mt-24">
         Kindly choose your current home size.
       </h1>
       <div className="max-w-xl mx-auto">
         {apartments.map((apartment) => (
           <div
             key={apartment.id}
-            className={`flex items-center justify-between w-full h-450 px-4 my-8 border-2 border-green-500 rounded-lg bg-white`}
+            className={`flex items-center justify-between w-full h-450 px-4 my-8 border-2 border-primary-green rounded-lg bg-white`}
             onClick={() => handleApartmentClick(apartment.id)}
           >
             <div className="flex-1 text-xl">
-              <p className={`font-bold ${selectedApartmentId === apartment.id ? "text-green-500" : "text-black"}`}>
+              <p className={`font-bold ${selectedApartmentId === apartment.id ? "text-primary-green" : "text-black"}`}>
                 {selectedApartmentId === apartment.id ? (
                   <span><FaCheck className="inline-block w-6 h-6 mr-2" />{apartment.size}</span>
                 ) : (
                   <span>{apartment.size}</span>
                 )}
               </p>
-              <p className={`${selectedApartmentId === apartment.id ? "text-green-500" : "text-black"}`}>{apartment.description}</p>
+              <p className={`${selectedApartmentId === apartment.id ? "text-primary-green" : "text-black"}`}>{apartment.description}</p>
             </div>
-            {selectedApartmentId === apartment.id ? null : <FaChevronRight className="w-8 h-8 text-green-500" />}
+            {selectedApartmentId === apartment.id ? null : <FaChevronRight className="w-8 h-8 text-primary-green" />}
           </div>
         ))}
       </div>
       <Link
         to="/search"
-        className={`bg-green-500 rounded-lg text-white font-bold py-2 px-4 bottom-16 right-16 absolute ${
+        className={`bg-primary-green rounded-lg text-white font-bold py-2 px-4 bottom-16 right-16 absolute ${
           selectedApartmentId ? "" : "opacity-50 cursor-not-allowed"
         }`}
         onClick={handleNextClick}
