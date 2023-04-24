617ed5e4a0f27b7a8c016533b60b4dbf849797c5
lizdmukami <lizdmukami@gmail.com>
2023-04-24 08:34:20 +0300

    fix: private routes and remove user on conditional statement

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -5,7 +5,7 @@ function PrivateRoute(props) {
   const location = useLocation();
   const { user } = useAuth();
   const token = localStorage.getItem("jwt");
-  if (!user || !token) {
+  if (!token) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
