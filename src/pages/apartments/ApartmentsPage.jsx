6e0f2a4dc2abfe5579779743630661a465c63433
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-24 15:16:09 +0300

    feat: Add icons for apartment selection and navigation

--- src/pages/apartments/ApartmentsPage.jsx
+++ src/pages/apartments/ApartmentsPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { FaCheck, FaChevronRight } from "react-icons/fa";
 
 function ApartmentsPage() {
   const [apartments, setApartments] = useState([]);
