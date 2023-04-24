55e7ce380faf45b0f2f3b828e18a58542a14f5ae
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-24 21:50:13 +0300

    feat:fetch boxes model from the backend

--- src/pages/boxes-page/BoxesRange.jsx
+++ src/pages/boxes-page/BoxesRange.jsx
@@ -1,19 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaArrowRight, FaCheck } from 'react-icons/fa';
 
 function BoxesRange() {
-    const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const [ranges, setRanges] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/boxes') // replace with your backend API endpoint
+      .then((response) => response.json())
+      .then((data) => setRanges(data));
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-    <div className="w-3/5 p-4">
-      <h1 className="text-3xl md:text-4xl font-bold text-grey-800 text-center mb-8">
-        Please select the number of boxes, bags or suit cases you want to move.
-      </h1>
-      <div className="flex flex-col">
-          {[1, 6, 11, 21, 31, 50].map((range, index) => (
+      <div className="w-3/5 p-4">
+        <h1 className="text-3xl md:text-4xl font-bold text-grey-800 text-center mb-8">
+          Please select the number of boxes, bags or suit cases you want to move.
+        </h1>
+        <div className="flex flex-col">
+          {ranges.map((range, index) => (
             <button
-              key={range}
+              key={range.id}
               className={`flex items-center justify-between w-full h-12 md:h-16 rounded-lg border-2 border-gray-200 mb-2 ${
                 index === selected
                   ? 'bg-green-500 border-green-500 text-white'
@@ -21,9 +28,7 @@ function BoxesRange() {
               }`}
               onClick={() => setSelected(index)}
             >
-              <span className="text-base md:text-lg ml-4">{`${range} - ${
-                index === 5 ? '+' : range + 4
-              }`}</span>
+              <span className="text-base md:text-lg ml-4">{range.range}</span>
               {index === selected ? (
                 <FaCheck className="text-xl md:text-2xl mr-4" />
               ) : (
@@ -38,8 +43,8 @@ function BoxesRange() {
           </button>
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
-export default BoxesRange
+export default BoxesRange;
