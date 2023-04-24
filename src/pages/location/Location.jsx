5b1b5dd9b1c77bf7b93a1a7b21d5b44e01d35d87
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-25 02:53:07 +0300

    feat: Add header with slogan

--- src/pages/location/Location.jsx
+++ src/pages/location/Location.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { FiArrowUp, FiChevronDown } from "react-icons/fi";
 function Location() {
   return (
-    <div>Location</div>
+    div className="min-h-screen flex flex-col justify-between bg-white m-0">
+       <header className="flex justify-center py-6 w-full">
+         <h1 className="text-4xl font-bold text-center text-green-500">
+           Move comfortably anywhere anytime
+         </h1>
+       </header>
   )
 }
 
