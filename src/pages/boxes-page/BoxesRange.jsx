f46d0b24320cf0e9ecf5236b1c3f29e7e7bf6b10
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-24 21:55:46 +0300

    fix:add top-margin

--- src/pages/boxes-page/BoxesRange.jsx
+++ src/pages/boxes-page/BoxesRange.jsx
@@ -12,7 +12,7 @@ function BoxesRange() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center mt-32 pt-8 h-screen">
       <div className="w-3/5 p-4">
         <h1 className="text-3xl md:text-4xl font-bold text-grey-800 text-center mb-8">
           Please select the number of boxes, bags or suit cases you want to move.
