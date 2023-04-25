1ee51799062aa9a15e19e2560846027c06ffb6ac
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-25 23:35:12 +0300

    feat: Add placeholder text for pickup time input field

--- src/pages/location/Location.jsx
+++ src/pages/location/Location.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { FiArrowUp, FiChevronDown } from "react-icons/fi";
-import { Link } from "react-router-dom";
+// import ReactDOM from "react-dom";
+import { FiChevronDown } from "react-icons/fi";
+//  import { Link } from "react-router-dom";
 
 function LocationPage() {
   const [pickupTime, setPickupTime] = useState("");
@@ -45,11 +46,11 @@ function LocationPage() {
                 onChange={handleTimeChange}
               />
             </div>
-            <Link to="/movers">
+      
               <button className="w-full md:w-auto h-16 py-4 px-6 text-lg text-white font-bold bg-green-500 rounded-full hover:bg-gray-800 focus:outline-none focus:shadow-outline-green">
                 Get Quote
               </button>
-            </Link>
+          
           </div>
         </div>
       </div>
@@ -58,41 +59,32 @@ function LocationPage() {
           Why choose us?
         </h2>
         <p className="text-lg text-center text-gray-600 mb-8">
-          We provide a seamless and stress-free moving experience for all your
-          belongings.
+          We provide a seamless and stress-free moving experience for all your belongings.
         </p>
         <ul className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
           <li className="bg-gray-200 rounded-lg shadow-md p-8">
             <h3 className="text-2xl font-bold text-green-500 mb-2">Safety First</h3>
             <p className="text-gray-700">
-              We prioritize the safety of your belongings and ensure they are
-              moved with the utmost care.
-             </p>
-           </li>
-           <li className="bg-gray-200 rounded-lg shadow-md p-8">
-             <h3 className="text-2xl font-bold text-green-500 mb-2">On-Time Delivery</h3>
- <p className="text-gray-700">
- We understand the importance of timely delivery and make sure your
- belongings reach their destination on time.
- </p>
- </li>
- <li className="bg-gray-200 rounded-lg shadow-md p-8">
- <h3 className="text-2xl font-bold text-green-500 mb-2">Professional Movers</h3>
- <p className="text-gray-700">
- Our team of professional movers is trained to handle all types of
- belongings and ensure they are transported safely.
- </p>
- </li>
- </ul>
- <div className="flex justify-center items-center bg-gray-200 rounded-full py-4 px-8 mb-12">
- <FiArrowUp className="text-green-500 text-2xl mr-2" />
- <p className="text-gray-700 font-bold text-lg">
- Book now and get 10% off on your first move!
- </p>
- </div>
- </div>
- </div>
+            We take safety seriously and ensure that all your belongings are transported securely and without damage. Our team is trained to handle all types of items, from fragile and delicate to bulky and heavy. We use high-quality packing materials and modern equipment to make sure your belongings are safe and secure during transit.
+</p>
+</li>
+<li className="bg-gray-200 rounded-lg shadow-md p-8">
+<h3 className="text-2xl font-bold text-green-500 mb-2">Affordable Prices</h3>
+<p className="text-gray-700">
+We believe that moving shouldn't cost a fortune. That's why we offer competitive and transparent pricing. Our rates are based on the distance, the size of the move, and any additional services you may need. We provide a detailed quote upfront so you know exactly what you'll be paying.
+</p>
+</li>
+<li className="bg-gray-200 rounded-lg shadow-md p-8">
+<h3 className="text-2xl font-bold text-green-500 mb-2">Professional Team</h3>
+<p className="text-gray-700">
+Our team of movers is experienced, reliable, and friendly. We take pride in providing excellent customer service and making your move as smooth as possible. We handle all aspects of your move, from packing and loading to unloading and unpacking. You can trust us to take care of everything.
+</p>
+</li>
+</ul>
+</div>
+</div>
+);
+}
 
- );
- }
- export default LocationPage;
+export default LocationPage
+
