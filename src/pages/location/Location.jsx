d31340c5b8b9a560de680af1b4bc9ff5810bfc77
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-04-25 23:41:52 +0300

    fix: modify the heading

--- src/pages/location/Location.jsx
+++ src/pages/location/Location.jsx
@@ -14,7 +14,7 @@ function LocationPage() {
     <div className="min-h-screen flex flex-col justify-between bg-white m-0">
       <header className="flex justify-center py-6 w-full">
         <h1 className="text-4xl font-bold text-center text-green-500">
-          Move comfortably anywhere anytime
+          Move comfortably, anywhere at anytime!
         </h1>
       </header>
       <div className="flex flex-col md:flex-row justify-center w-full px-8">
